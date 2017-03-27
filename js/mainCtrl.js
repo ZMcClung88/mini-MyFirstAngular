01b7d175039fd1e6f47209e38401e91b2981e0c4
Zac McClung <zacmcclung@Zacs-MacBook-Pro.local>
2017-03-27 14:03:44 -0600

    adding and saving files -- did not complete step 6

---
+++ js/mainCtrl.js
@@ -0,0 +1,16 @@
+angular.module('friendsList')
+	   .controller('mainCtrl', function($scope) {
+
+	   $scope.name = 'Zac',
+
+	   $scope.friend = [
+	   		'Courtney',
+	   		'Mayble',
+	   		'Izzy',
+	   		'Caleb',
+	   		'Dedrick',
+	   		'Steve',
+	   		'Andrew'
+	   ];
+
+});
